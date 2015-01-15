a010658ee5dcd890a26b2c3e7fb720fbb2799c35
aaronksaunders <aaron@clearlyinnovative.com>
2015-01-14 22:46:10 -0500

    added some tests to show the direction I think you should be heading

--- test-acl.js
+++ test-acl.js
@@ -5,11 +5,55 @@
 
 var acl = require('acl');
 
+// Or Using the memory backend
+myACL = new acl(new acl.memoryBackend());
 
-//Using the memory backend
-acl = new acl(new acl.memoryBackend());
-acl.allow('guest', 'blogs', 'view');
+// create role/ACL for a specific concept(s) using the concept id
+// as part of the name for simplification
+myACL.allow('concept-123', 'concepts/123', ['get','put','post'])
+myACL.allow('concept-125', 'concepts/125', ['get','put','post'])
 
-acl.addUserRoles('joed', 'guest');
+// create a member role, just to demonstrate multiple roles
+myACL.allow('member', 'members-only', ['get','put','post'])
 
-console.log(acl.userRoles('joed'));
+// add roles to a specific user - joed
+myACL.addUserRoles('joed', 'member');
+myACL.addUserRoles('joed', 'concept-123');
+
+// add roles to a specific user - aaron
+myACL.addUserRoles('aaron', 'member');
+myACL.addUserRoles('aaron', 'super-admin');
+myACL.addUserRoles('aaron', 'concept-125');
+
+
+// Helper functions..
+function dumpUserRoles(_user) {
+	myACL.userRoles(_user, function(err, res) {
+    	err && console.error(JSON.stringify(err));
+   		console.log('roles: ' + _user + " " +  JSON.stringify(res));
+	});
+}
+
+function dumpRoleResources(_role) {
+	myACL.whatResources(_role, function(err, res) {
+
+    	err && console.error(JSON.stringify(err));
+   		console.log('whatResources: '+ _role + " " + JSON.stringify(res));
+	});
+}
+
+// Test Output
+
+dumpRoleResources("concept-123");
+dumpRoleResources("concept-125");
+
+dumpUserRoles('joed');
+dumpUserRoles('aaron');
+
+// test access to a specific url/path
+myACL.isAllowed('joed', 'concepts/125', 'put', function(err, res) {
+	err && console.error(JSON.stringify(err));
+   	console.log('isAllowed: joed - concepts/125 ' + JSON.stringify(res));
+});
+
+
